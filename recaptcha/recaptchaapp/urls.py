72b90fb27780b836a1c0d993291c21756616fc95
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-05-01 06:41:38 +0530

    ok

--- recaptcha/recaptchaapp/urls.py
+++ recaptcha/recaptchaapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.submitform,name='submitform'),
-    path('/index',views.index,name='index')
+    path('index/',views.index,name='index'),
+    path('verify_recaptcha/',views.verify_recaptcha,name='verify_recaptch')
 ]
--- recaptcha/recaptchaapp/views.py
+++ recaptcha/recaptchaapp/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render,redirect
-
-
-
+from django.http import JsonResponse
+import requests
 def submitform(request):
     return render(request,'form.html')
 
 def index(request):
+    if request.method =="POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        place = request.POST.get("place")
+        print(name,age,place)
     return redirect(submitform)
+
+def verify_recaptcha(request):
+    recaptcha_response= request.POST.get("recaptchaResponse")
+    secret_key="6Le9jncpAAAAAPYWAaCJCwjtORLoZFH-cJ6KY1aX"
+    try:
+        response =requests.post(
+            "https://www.google.com/recaptcha/api/siteverify",
+            data={"secret":secret_key,"response":recaptcha_response}
+        )
+        data = response.json()
+    except:
+        data=None
+    return JsonResponse(data)    
