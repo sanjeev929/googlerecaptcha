72e04c3140495bec2c3f6886af1306ef9f321c04
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-05-02 13:03:21 +0530

    sansj

--- recaptcha/recaptcha/settings.py
+++ recaptcha/recaptcha/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ka2h=ufr25!l1f@iya%31_dzl30!cal)w)c1!attzpvccznp6m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
