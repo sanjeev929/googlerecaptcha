4041e733db76c61a70f1b551594e5a2396cdadb7
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-05-02 12:06:25 +0530

    sansj

--- recaptcha/recaptchaapp/views.py
+++ recaptcha/recaptchaapp/views.py
@@ -10,7 +10,7 @@ def index(request):
         age = request.POST.get("age")
         place = request.POST.get("place")
         print(name,age,place)
-        print("okkkkkk")
+        print("okkkkk")
     return redirect(submitform)
 
 def verify_recaptcha(request):
